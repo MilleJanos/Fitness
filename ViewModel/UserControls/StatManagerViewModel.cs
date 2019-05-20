0e86ba1436bb961d83cbda216ab4a97d5dcca291
andrea <andrea@github.com>
2019-05-20 23:52:37 +0300

    Statisztika, nem vegso

--- ViewModel/UserControls/StatManagerViewModel.cs
+++ ViewModel/UserControls/StatManagerViewModel.cs
@@ -12,6 +12,59 @@ namespace ViewModel.UserControls
 {
     public class StatManagerViewModel : ViewModelBase, IStatManagerContent
     {
+        private int _totalCount;
+        private int _count_6_10;
+        private int _count_10_14;
+        private int _count_14_20;
+        private int _count_20_6;
+
+        public int Count_20_6
+        {
+            get { return _count_20_6; }
+            set
+            {
+                _count_20_6 = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public int Count_14_20
+        {
+            get { return _count_14_20; }
+            set { _count_14_20 = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        public int Count_10_14
+        {
+            get { return _count_10_14; }
+            set { _count_10_14 = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+
+        public int Count_6_10
+        {
+            get { return _count_6_10; }
+            set {
+                _count_6_10 = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        public int TotalCount
+        {
+            get { return _totalCount; }
+            set { _totalCount = value;
+                RaisePropertyChanged();
+            }
+        }
+
 
         private DateTime _date;
 
@@ -26,7 +79,8 @@ namespace ViewModel.UserControls
         public StatManagerViewModel()
         {
             this.CloseTabItemCommand = new RelayCommand(this.CloseTabItemExecute);
-            User user = new User();
+            Entry entry = new Entry();
+            CalculateStatistic();
 
         }
 
@@ -42,10 +96,15 @@ namespace ViewModel.UserControls
             MainWindowViewModel.Instance.CloseTabItem(this);
         }
 
-       /* public List<User> Counter(User user)
+       public void CalculateStatistic()
         {
-            return user;
+            TotalCount = Fitness.Logic.Data.FitnessC.GetEntryes().Count();
+            Count_6_10 = Fitness.Logic.Data.FitnessC.GetEntryes().Where(e=>e.Date.Hour>=6 && e.Date.Hour<10).Count();
+            Count_10_14 = Fitness.Logic.Data.FitnessC.GetEntryes().Where(e=>e.Date.Hour>=10 && e.Date.Hour<14).Count();
+            Count_14_20 = Fitness.Logic.Data.FitnessC.GetEntryes().Where(e=>e.Date.Hour>=14 && e.Date.Hour<=20).Count();
+            Count_20_6 = Fitness.Logic.Data.FitnessC.GetEntryes().Where(e=>e.Date.Hour>11 && e.Date.Hour<12).Count();
+
         }
-        */
+        
     }
 }
